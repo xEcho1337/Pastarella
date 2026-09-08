f060555b7f96912d3831aad09d9d648a7f5ce91d
GiacomoMarinaio <145773929+GiacomoMarinaio@users.noreply.github.com>
2026-09-08 11:33:25 +0200

    feat(web): improved main page

--- web/bootstrap_auto_theme.js
+++ web/bootstrap_auto_theme.js
@@ -1,9 +1,20 @@
-const htmlElement = document.querySelector("html")
-if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
-	function updateTheme() {
-		document.querySelector("html").setAttribute("data-bs-theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-	}
+const htmlElement = document.querySelector("html");
 
-	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
-	updateTheme();
+if (htmlElement.getAttribute("data-bs-theme") === "auto") {
+    function updateTheme() {
+        htmlElement.setAttribute(
+            "data-bs-theme",
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "dark"
+                : "light"
+        );
+    }
+
+    const darkModeQuery = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+    );
+
+    darkModeQuery.addEventListener("change", updateTheme);
+
+    updateTheme();
 }
