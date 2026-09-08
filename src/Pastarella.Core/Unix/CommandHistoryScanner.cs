130e401b6ca60783bf7e2f3f977bcbe5e0ac0391
reokodoku <reokodoku@lyboc.dns.navy>
2026-09-08 17:28:12 +0200

    feat(Unix): add command history scanner

--- src/Pastarella.Core/Unix/CommandHistoryScanner.cs
+++ src/Pastarella.Core/Unix/CommandHistoryScanner.cs
@@ -6,6 +6,15 @@ public class CommandHistoryScanner : ICommandHistoryScanner
 {
     public IEnumerable<CommandHistory> Scan()
     {
-        throw new NotImplementedException();
+        string home = Environment.GetEnvironmentVariable("HOME") ?? throw new Exception("HOME env not found");
+
+        if (File.Exists($"{home}/.ash_history"))
+            yield return new("ash", File.ReadAllLines($"{home}/.ash_history"));
+
+        if (File.Exists($"{home}/.bash_history"))
+            yield return new("bash", File.ReadAllLines($"{home}/.bash_history"));
+
+        if (File.Exists($"{home}/.zsh_history"))
+            yield return new("zsh", File.ReadAllLines($"{home}/.zsh_history"));
     }
 }
