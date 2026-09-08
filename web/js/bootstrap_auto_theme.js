65f9efa1d327d073d9a275472fc5149c42890cdc
reokodoku <reokodoku@lyboc.dns.navy>
2026-09-08 12:42:16 +0200

    feat(web): add toggle theme icon

--- web/js/bootstrap_auto_theme.js
+++ web/js/bootstrap_auto_theme.js
@@ -1,9 +1,32 @@
-const htmlElement = document.querySelector("html")
-if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
-	function updateTheme() {
-		document.querySelector("html").setAttribute("data-bs-theme", window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-	}
+function updateTheme(theme) {
+	document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", theme);
 
-	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
-	updateTheme();
+	let icon = document.getElementById("theme-icon");
+	switch (theme) {
+		case "light":
+			icon.classList.remove("bi-sun-fill");
+			icon.classList.add("bi-moon-fill");
+			break;
+		case "dark":
+			icon.classList.remove("bi-moon-fill");
+			icon.classList.add("bi-sun-fill");
+			break;
+	}
 }
+
+function toggleTheme() {
+	switch (document.getElementsByTagName("html")[0].getAttribute("data-bs-theme")) {
+		case "light":
+			updateTheme("dark");
+			break;
+		case "dark":
+			updateTheme("light");
+			break;
+	}
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+	updateTheme(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+});
+
+updateTheme(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
