c5650e6b30bc6a01a4a421f5f0cf0bc867d57527
xEcho1337 <123984252+xEcho1337@users.noreply.github.com>
2026-09-10 12:18:24 +0200

    feat: removed section header in report if no data is found

--- src/Pastarella.Terminal/Outputs/TxtWriter.cs
+++ src/Pastarella.Terminal/Outputs/TxtWriter.cs
@@ -29,45 +29,79 @@ public class TxtWriter
         _buffer.WriteLine($"###### Report timestamp: {report.Timestamp} ######");
 
         var forensic = new ForensicServices(_buffer);
-        PrintTitle("Processes");
-        forensic.WriteProcesses(report.Processes);
 
-        PrintTitle("Services");
-        forensic.WriteServices(report.Services);
+        if (report.Processes.Count != 0)
+        {
+            PrintTitle("Processes");
+            forensic.WriteProcesses(report.Processes);
+        }
 
-        PrintTitle("Users");
-        forensic.WriteUsers(report.Users);
+        if (report.Services.Count != 0)
+        {
+            PrintTitle("Services");
+            forensic.WriteServices(report.Services);
+        }
 
-        PrintTitle("Storages");
-        forensic.WriteStorages(report.Storages);
+        if (report.Users.Count != 0)
+        {
+            PrintTitle("Users");
+            forensic.WriteUsers(report.Users);
+        }
 
-        var network = new NetworkService(_buffer);
-        PrintTitle("Open connections");
-        network.WritePorts(report.OpenPorts);
+        if (report.Storages.Count != 0)
+        {
+            PrintTitle("Storages");
+            forensic.WriteStorages(report.Storages);
+        }
 
-        var hosts = new HostsScanner(_buffer);
-        PrintTitle($"Hosts file ({Core.Common.HostsScanner.Path})");
-        hosts.WriteHosts(report.Hosts);
+        if (report.OpenPorts.Count != 0)
+        {
+            var network = new NetworkService(_buffer);
+            PrintTitle("Open connections");
+            network.WritePorts(report.OpenPorts);
+        }
 
-        var persistences = new PersistenceScanner(_buffer);
-        PrintTitle("Persistences");
-        persistences.WritePersistences(report.Persistences);
+        if (report.Hosts.Count != 0)
+        {
+            var hosts = new HostsScanner(_buffer);
+            PrintTitle($"Hosts file ({Core.Common.HostsScanner.Path})");
+            hosts.WriteHosts(report.Hosts);
+        }
 
-        var drivers = new DriverScanner(_buffer);
-        PrintTitle("Drivers");
-        drivers.WriteDrivers(report.Drivers);
+        if (report.Persistences.Count != 0)
+        {
+            var persistences = new PersistenceScanner(_buffer);
+            PrintTitle("Persistences");
+            persistences.WritePersistences(report.Persistences);
+        }
 
-        var envs = new EnvironmentVariablesScanner(_buffer);
-        PrintTitle("Environment variables");
-        envs.WriteEnvs(report.Envs);
+        if (report.Drivers.Count != 0)
+        {
+            var drivers = new DriverScanner(_buffer);
+            PrintTitle("Drivers");
+            drivers.WriteDrivers(report.Drivers);
+        }
 
-        var cmdHistories = new CommandHistoryScanner(_buffer);
-        PrintTitle("Command Histories");
-        cmdHistories.WriteHistories(report.CommandHistories);
+        if (report.Envs.Count != 0)
+        {
+            var envs = new EnvironmentVariablesScanner(_buffer);
+            PrintTitle("Environment variables");
+            envs.WriteEnvs(report.Envs);
+        }
 
-        var recentFiles = new RecentFileScanner(_buffer);
-        PrintTitle("Recent Files (last 30 days)");
-        recentFiles.Write(report.RecentFiles);
+        if (report.CommandHistories.Count != 0)
+        {
+            var cmdHistories = new CommandHistoryScanner(_buffer);
+            PrintTitle("Command Histories");
+            cmdHistories.WriteHistories(report.CommandHistories);
+        }
+
+        if (report.RecentFiles.Count != 0)
+        {
+            var recentFiles = new RecentFileScanner(_buffer);
+            PrintTitle("Recent Files (last 30 days)");
+            recentFiles.Write(report.RecentFiles);
+        }
 
         return _buffer.Text;
     }
