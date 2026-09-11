fc9fea68a2b877bc35f968fbc09aa1f92a4185e7
reokodoku <reokodoku@lyboc.dns.navy>
2026-09-11 17:14:05 +0200

    fix(windows/CommandHistory): check if history file exist

--- src/Pastarella.Core/Windows/CommandHistoryScanner.cs
+++ src/Pastarella.Core/Windows/CommandHistoryScanner.cs
@@ -4,14 +4,18 @@ namespace Pastarella.Core.Windows;
 
 public class CommandHistoryScanner : ICommandHistoryScanner
 {
-    private static CommandHistory GetPowerShellHistory()
+    private static CommandHistory? GetPowerShellHistory()
     {
         string historyFilePath = $"{Environment.GetEnvironmentVariable("APPDATA")}\\Microsoft\\Windows\\PowerShell\\PSReadLine\\ConsoleHost_history.txt";
+        if (!File.Exists(historyFilePath))
+            return null;
         return new("PowerShell", File.ReadAllLines(historyFilePath));
     }
 
     public IEnumerable<CommandHistory> Scan(IProgress<ScanProgress>? progress = null)
     {
-        return [GetPowerShellHistory()];
+        if (GetPowerShellHistory() is CommandHistory hist)
+            return [hist];
+        return [];
     }
 }
