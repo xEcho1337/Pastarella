8c8f19c7915b076c4f5ca605a6c26b0843bbad76
reokodoku <reokodoku@lyboc.dns.navy>
2026-09-13 16:24:50 +0200

    chore(build): add musl targets for linux

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ dotnet publish -c Release -r win-arm64   -o ../../out/win-arm64
 dotnet publish -c Release -r win-x64     -o ../../out/win-x64
 dotnet publish -c Release -r osx-arm64   -o ../../out/osx-arm64
 dotnet publish -c Release -r linux-x64   -o ../../out/linux-x64
+dotnet publish -c Release -r linux-musl-x64   -o ../../out/linux-musl-x64
 dotnet publish -c Release -r linux-arm64 -o ../../out/linux-arm64
+dotnet publish -c Release -r linux-musl-arm64 -o ../../out/linux-musl-arm64
 
 echo "Done!"
