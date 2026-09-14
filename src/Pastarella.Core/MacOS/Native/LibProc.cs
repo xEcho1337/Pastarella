87d66b64cc8e5f8ed5a74ec936718f07d5e89a39
xEcho1337 <123984252+xEcho1337@users.noreply.github.com>
2026-09-14 11:39:10 +0200

    refactor(macos): simplify Native attributes with auto-property constructors

--- src/Pastarella.Core/MacOS/Native/LibProc.cs
+++ src/Pastarella.Core/MacOS/Native/LibProc.cs
@@ -26,35 +26,14 @@ public static unsafe class LibProc
     public static extern int proc_pidpath(int pid, void* buffer, [NativeTypeName("uint32_t")] uint buffersize);
 }
 
-internal sealed class NativeTypeNameAttribute : Attribute
+internal sealed class NativeTypeNameAttribute(string name) : Attribute
 {
-    private readonly string _name;
-
-    /// <summary>Initializes a new instance of the <see cref="NativeTypeNameAttribute" /> class.</summary>
-    /// <param name="name">The name of the type that was used in the native signature.</param>
-    public NativeTypeNameAttribute(string name)
-    {
-        _name = name;
-    }
-
-    /// <summary>Gets the name of the type that was used in the native signature.</summary>
-    public string Name => _name;
+    public string Name => name;
 }
 
-/// <summary>Defines the annotation found in a native declaration.</summary>
 [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
 [Conditional("DEBUG")]
-internal sealed class NativeAnnotationAttribute : Attribute
+internal sealed class NativeAnnotationAttribute(string annotation) : Attribute
 {
-    private readonly string _annotation;
-
-    /// <summary>Initializes a new instance of the <see cref="NativeAnnotationAttribute" /> class.</summary>
-    /// <param name="annotation">The annotation that was used in the native declaration.</param>
-    public NativeAnnotationAttribute(string annotation)
-    {
-        _annotation = annotation;
-    }
-
-    /// <summary>Gets the annotation that was used in the native declaration.</summary>
-    public string Annotation => _annotation;
+    public string Annotation => annotation;
 }
